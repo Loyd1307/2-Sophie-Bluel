0ba9a38852b568180f3c22ff37221b9d3e6c56ed
Loyd <bntplmghk@gmail.com>
2026-08-26 14:31:17 +0200

    recuperation donnees api + css galerie

---
+++ FrontEnd/api-request.js
@@ -0,0 +1,28 @@
+
+const gallery = document.querySelector(".gallery");
+
+async function getPhotos() {
+
+     const response = await fetch("http://localhost:5678/api/works");
+     const data = await response.json();
+     
+      data.forEach(photo => {
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const image = document.createElement("img");
+      image.src = photo.imageUrl;
+      image.alt = photo.title;
+
+      const title = document.createElement("p");
+      title.textContent = photo.title;
+
+      card.appendChild(image);
+      card.appendChild(title);
+
+      gallery.appendChild(card);
+    });
+}
+
+getPhotos();
