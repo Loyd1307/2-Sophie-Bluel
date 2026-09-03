76f03528a3609259ab24dc16a5f2f3c1e65707ec
Loyd <bntplmghk@gmail.com>
2026-09-03 16:35:22 +0200

    creation 2eme modal + debut js + debut css

--- FrontEnd/add-photo.js
+++ FrontEnd/add-photo.js
@@ -2,5 +2,31 @@
 const addPhotoButton = document.getElementById("addPhoto");
 
 addPhotoButton.addEventListener("click", function () {
-    const modalGallery = document.getElementById("modalGallery");
-    const addPhotoForm = document.getElementById("addPhotoForm");
+    const modal2 = document.getElementById("modal2");
+    modal2.style.display = "flex";
+    modal2.removeAttribute("aria-hidden");
+    modal2.setAttribute("aria-modal", "true");
+});
+
+addPhotoButton()
+
+ async function getCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
+
+    const categories = await response.json();
+
+    console.log(categories);
+}
+
+getCategories();
+
+const categorySelect = document.querySelector("#category");
+
+categories.forEach(category => {
+    const option = document.createElement("option");
+
+    option.value = category.id;
+    option.textContent = category.name;
+
+    categorySelect.appendChild(option);
+});
